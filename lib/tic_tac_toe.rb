79f01a5653a78a3dc79eeb4e237b7ad13a0234f3
Evan Burton <evan@heyevan.com>
2020-05-08 06:57:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,6 +42,11 @@ def position_taken?(index) # code your #position_taken? method here!
   end
 end
 
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board,index)
+    return true
+  end
+end
 
 
 
